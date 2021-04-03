dbb48c9dad6007866e9be0464b0fc5e9c9c57c32
MaxNollet <mjh.nollet@student.han.nl>
2021-04-03 15:31:16 +0200

    Class 'ResultServlet' geannoteerd, closes #19

--- src/main/java/servlets/ResultServlet.java
+++ src/main/java/servlets/ResultServlet.java
@@ -11,10 +11,23 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
 
-
-//TODO: documentatie toevoegen.
+/**
+ * Class ResultServlet
+ *
+ * A class which processes incoming requests from
+ * clients to show the results of a specific ORF-
+ * search.
+ *
+ * @version 1
+ * @author Yuri, Janneke & Max
+ * */
 public class ResultServlet extends HttpServlet {
-    //TODO: documentatie toevoegen.
+    /**
+     * A method which handles get-requests from clients. Shows
+     * the results of a specific ORF-search.
+     * @param request Incoming request from the client.
+     * @param response Outgoing response to the client.
+     * */
     @Override
     protected void doGet(HttpServletRequest request,
                          HttpServletResponse response)
@@ -77,7 +90,13 @@ public class ResultServlet extends HttpServlet {
         }
     }
 
-    //TODO: documentatie toevoegen.
+    /**
+     * A method which handles post-requests from clients. Handles
+     * requests to execute a BLAST-search or to view BLAST-search-
+     * results for this ORF-search.
+     * @param request Incoming request from the client.
+     * @param response Outgoing response to the client.
+     * */
     protected void doPost(HttpServletRequest request,
                           HttpServletResponse response)
             throws ServletException, IOException {
